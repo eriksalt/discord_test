d5baaa18be298cf41a5638e3c9c0ad55e2046bac
eriksalt <erik.saltwell@gmail.com>
2021-05-23 15:41:42 -0700

    added simple test commands

--- commands.py
+++ commands.py
@@ -1,12 +1,27 @@
 import os
 import discord
+from discord.ext import commands
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+client = commands.Bot(command_prefix=".")
+token = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
 
 @client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+async def on_ready() :
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
+    print("I am online")
 
-client.run(TOKEN)
+@client.command()
+async def ping(ctx) :
+    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+
+@client.command(name="whoami")
+async def whoami(ctx) :
+    await ctx.send(f"You are {ctx.message.author.name}")
+
+@client.command()
+async def clear(ctx, amount=3) :
+    await ctx.channel.purge(limit=amount)
+
+
+client.run(token)
